dcf8b10d2ed648daf1fc0041b3a5cb1bf79b224b
Trevor Ludlam <71052042+TrevorLudlam@users.noreply.github.com>
2022-04-01 16:54:38 -0400

    strings for import
    can now parse imported strings

--- node/game/cube_import.js
+++ node/game/cube_import.js
@@ -1,8 +1,28 @@
-exports.load_cube = function(cube_data) {
+exports.load_cube = function(cubeData) {
 
     if(true){
         //Return ok: true if the cube is valid. Return data if necessary
-        return { ok: true }
+
+        let strArray = cubeData.split("\n");
+        let num = 0;
+        let cardName = '';
+        let cardArray = [];
+        let errorString = "";
+        for(elem in strArray){
+            num = elem.slice(0, elem.indexOf(' ')-1);
+            num = parseInt(num, 10);
+            if(num == NaN){
+                errorString = "String" + elem + "is not an acceptable input.";
+                break;
+            }
+            cardName = elem.slice(elem.indexOf(' ')+1, elem.length - 1);
+            cardArray.push({num, cardName});
+        }
+        if(errorString != ""){
+            return { ok: false, error: errorString }
+        }
+
+        return { ok: true, cardArray: cardArray }
     } else {
         //Return ok: false if the cube is not valid (Too few cards)
         return { ok: false }
