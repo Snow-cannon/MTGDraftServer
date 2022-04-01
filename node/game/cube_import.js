b978f132858e386a8448e9ed09f5064f87af1153
Trevor Ludlam <71052042+TrevorLudlam@users.noreply.github.com>
2022-04-01 17:07:58 -0400

    might actually work this time
    parse strings from given input, now with more error handling

--- node/game/cube_import.js
+++ node/game/cube_import.js
@@ -1,30 +1,23 @@
 exports.load_cube = function(cubeData) {
 
-    if(true){
-        //Return ok: true if the cube is valid. Return data if necessary
-
-        let strArray = cubeData.split("\n");
-        let num = 0;
-        let cardName = '';
-        let cardArray = [];
-        let errorString = "";
-        for(elem in strArray){
-            num = elem.slice(0, elem.indexOf(' ')-1);
-            num = parseInt(num, 10);
-            if(num == NaN){
-                errorString = "String" + elem + "is not an acceptable input.";
-                break;
-            }
-            cardName = elem.slice(elem.indexOf(' ')+1, elem.length - 1);
-            cardArray.push({num, cardName});
-        }
-        if(errorString != ""){
-            return { ok: false, error: errorString }
-        }
-
-        return { ok: true, cardArray: cardArray }
-    } else {
-        //Return ok: false if the cube is not valid (Too few cards)
-        return { ok: false }
+    //Return ok: true if the cube is valid. Return data if necessary
+    if(cubeData === ""){
+        return { ok: false, error: "String is empty" }
     }
+    let strArray = cubeData.split(/\r?\n/);
+    let num = 0;
+    let cardName = '';
+    let cardArray = [];
+    for(elem in strArray){
+        num = elem.slice(0, elem.indexOf(' ')-1);
+        num = parseInt(num, 10);
+        if(num == NaN){
+            return { ok: false, error: "String " + elem + " is not an acceptable input." }
+        }
+        cardName = elem.slice(elem.indexOf(' ')+1, elem.length - 1);
+        cardArray.push({num, cardName});
+    }
+
+    return { ok: true, cardArray: cardArray }
+
 }
